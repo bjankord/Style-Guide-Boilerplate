ddfb8b5a86e254cd47b1519a8a57180149974bc7
Brett Jankord <bjankord@gmail.com>
2013-03-25 12:44:56 -0500

    Update 0.0.1
    * remove dependancy on jQuery
    * locally host highlight.js files
    * use placedog.com instead of lorempixel.com

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 <meta charset="utf-8">
   <title>Style Guide Boilerplate</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width">
   
   <!-- Style Guide Boilerplate Styles -->
   <link rel="stylesheet" href="css/sg-style.css">
@@ -10,23 +10,19 @@
   
   <!-- Replace below stylesheet with your own stylesheet -->
   <link rel="stylesheet" href="css/style.css">
-
-  <!-- Highlight.js Styles and Scripts -->
-  <link rel="stylesheet" href="http://softwaremaniacs.org/media/soft/highlight/styles/github.css">
-  <script src="http://yandex.st/highlightjs/7.3/highlight.min.js"></script>
 </head>
-<body>
+<body id="page">
 
 <div class="sg-header">
   <div class="sg-container">
-      <a href="#nav" class="sg-nav-toggle"><img src="images/navicon.png" width="30" alt=""></a>
+    <a href="#nav" id="sg-nav-toggle" class="sg-nav-toggle">&#9776;</a>
     <span class="sg-logo">STYLE GUIDE</span>
   </div><!--/.sg-container-->
 </div><!--/.sg-header-->
 
-<div class="sg-wrap">
-  
-  <nav class="sg-nav" role="navigation">
+<div id="sg-wrap" class="sg-wrap">
+
+  <nav id="sg-nav" class="sg-nav" role="navigation">
     <div class="sg-nav-inner">
       <h2>Base Styles</h2>
       <ul>
@@ -72,10 +68,10 @@
       </ul>
     </div>
   </nav>
-  
-  <div class="sg-main sg-container" role="main">
+
+  <div class="sg-container">
     <div class="sg-intro">               
-      <h1 class="sg-heading-primary">Your Site's Style Guide</h1>
+      <h1>Your Site's Style Guide</h1>
 
       <p>Comments and documentation about your style guide. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus nobis enim labore facilis consequuntur! Veritatis neque est suscipit tenetur temporibus enim consequatur deserunt perferendis. Neque nemo iusto minima deserunt amet.<p>
 
@@ -104,7 +100,7 @@
     </div><!--/.sg-intro-->    
 
     <div class="sg-styles sg-styles-base">    
-      <h1 class="sg-heading-primary">Base Styles</h1>
+      <h1 class="pattern-main-heading">Base Styles</h1>
       <?php
       $files = array();
       $handle=opendir('markup/base');
@@ -118,12 +114,12 @@
       foreach ($files as $file):
           $filename = preg_replace("/\.html$/i", "", $file);
           $title = preg_replace("/\-/i", " ", $filename);
-          echo '<div class="sg-pattern">';
-          echo '<div class="sg-display">';
-          echo '<h2 class="sg-heading-secondary"><a name="'.$filename.'">'.$title.'</a></h2>';
+          echo '<div class="pattern">';
+          echo '<div class="display">';
+          echo '<h2 class="pattern-heading"><a name="'.$filename.'">'.$title.'</a></h2>';
           include('markup/base/'.$file);
           echo '</div>';
-          echo '<div class="sg-source">';
+          echo '<div class="source">';
           echo '<p><a href="markup/base/'.$file.'">'.$file.'</a></p>';
           echo '<pre><code>';
           echo htmlspecialchars(file_get_contents('markup/base/'.$file));
@@ -135,7 +131,7 @@
     </div>
 
     <div class="sg-styles sg-styles-patterns">
-      <h1 class="sg-heading-primary">Pattern Styles</h1>
+      <h1>Pattern Styles</h1>
       <h2>Design and mark-up patterns unique to your site.</h2>
       <?php
       $files = array();
@@ -150,12 +146,12 @@
       foreach ($files as $file):
           $filename = preg_replace("/\.html$/i", "", $file);
           $title = preg_replace("/\-/i", " ", $filename);
-          echo '<div class="sg-pattern">';
-          echo '<div class="sg-display">';
-          echo '<h2 class="sg-heading-secondary"><a name="'.$filename.'">'.$title.'</a></h2>';
+          echo '<div class="pattern">';
+          echo '<div class="display">';
+          echo '<h2 class="pattern-heading"><a name="'.$filename.'">'.$title.'</a></h2>';
           include('markup/patterns/'.$file);
           echo '</div>';
-          echo '<div class="sg-source">';
+          echo '<div class="source">';
           echo '<p><a href="markup/patterns/'.$file.'">'.$file.'</a></p>';
           echo '<pre><code>';
           echo htmlspecialchars(file_get_contents('markup/patterns/'.$file));
@@ -168,7 +164,7 @@
   </div><!--/.container-->
 </div><!--/.wrap-->
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+<script src="js/highlight.min.js"></script>
 <script src="js/sg-scripts.js"></script>
 </body>
 </html>
