937e47ff2fe8f8583189c3ad3f461708d62b95fd
Geoff Young <geoffreysyoung@gmail.com>
2013-07-12 19:58:42 -0400

    Add button that selects sample markup to be copied

--- js/sg-scripts.js
+++ js/sg-scripts.js
@@ -27,6 +27,21 @@
       hasClass(el, cl) ? removeClass(el, cl) : addClass(el, cl);
   };
 
+  var selectText = function(text) {
+      var doc = document;
+      if (doc.body.createTextRange) {
+          var range = doc.body.createTextRange();
+          range.moveToElementText(text);
+          range.select();
+      } else if (window.getSelection) {
+          var selection = window.getSelection();
+          var range = doc.createRange();
+          range.selectNodeContents(text);
+          selection.removeAllRanges();
+          selection.addRange(range);
+      }
+  };
+
 
   // Cut the mustard
   if ( !Array.prototype.forEach ) {
@@ -42,6 +57,14 @@
         var that = this;
         var sourceCode = that.parentNode.nextElementSibling;
         toggleClass(sourceCode, 'is-active');
+        return false;
+      };
+    }, false);
+
+    // Select Code Button
+    [].forEach.call( document.querySelectorAll('.sg-btn--select'), function(el) {
+      el.onclick = function() {
+        selectText(this.nextSibling);
         return false;
       };
     }, false);
