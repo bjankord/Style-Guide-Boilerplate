368446a26765ab7f0b6877fc18504bbd8cba94aa
Kemie Guaida <kemie@monolinea.com>
2015-01-18 15:21:43 +0100

    added usage files, misc fixes
    1.1.0 / 2015-01-18
    ==================
    
      * Added the ability to add usage notes for elements
      * Moved code to functions.php
      * Cleaned markup for wells, buttons & advanced buttons
      * Style fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,123 +1,40 @@
-2.3.0 / 2013-08-24
+1.1.0 / 2015-01-18
 ==================
 
- * Update address markup to more neutral base
-
-2.2.0 / 2013-08-24
-==================
-
- * Update wording on button that selects sample markup to be copied
- * Add tooltip to copy source button
-
-2.1.0 / 2013-07-13
-==================
-
- * Add button that selects sample markup to be copied
- * Resolves issue #4 and issue #6 
-
-2.0.2 / 2013-07-09
-==================
-
- * Resolved issue #3 
-
-2.0.1 / 2013-07-09
-==================
-
- * Removed custom scrollbar styles
-
-2.0.0 / 2013-06-18
-==================
- 
- * Reworked navigation
- * Reworked layout
- * Added hex values to color swatches
- * Added ability to toggle code snippets
- * Switched to placehold.it for images
- * Switched to prettify.js for code highlighting
- * Improved usability across mobile and legacy browsers
- * Normalized time element formatting
-
-1.9.0 / 2013-06-07
-==================
- 
- * Normalize formatting for all markup snippets
- * Update tabular data example
-
-1.8.1 / 2013-05-29
-==================
- 
- * Update sources for video and audio elements
-
-1.8.0 / 2013-05-29
-==================
-
- * Reworked Media section
- * Add Audio example
- * Add Video example
- * Reworked preformatted text
- * Reworked tabular data
- * Add table with side headings
-
-1.7.1 / 2013-05-28
-==================
+  * Added the ability to add usage notes for elements
+  * Moved code to functions.php
+  * Cleaned markup for wells, buttons & advanced buttons
+  * Style fixes
   
- * Fix dynamic output value on range slider
 
-1.7.0 / 2013-05-24
+1.0.4 / 2014-11-18
 ==================
+
+  * Fixed incorrect padding /nesting
+
+1.0.3 / 2014-07-25
+==================
+
+  * Deleted unneccesary files
+
   
- * Add datalist fields
-
-1.6.0 / 2013-05-24
-==================
- 
- * Reworked Form Buttons section
- * Add disabled buttons
- * Add fieldset examples
- * Reworked Form Fields
- * Add speech recognition input
- * Add time input
- * Add disabled fields
- * Add readonly fields
-
-1.5.0 / 2013-05-24
-==================
- 
- * Add meter element
- * Add progress element
- 
-1.4.0 / 2013-05-24
-==================
- 
- * Add details element
- 
-1.3.0 / 2013-05-24
+1.0.2 / 2014-04-15
 ==================
 
- * Remove headings in hgroup
- * Add small text and link in heading examples
- * Add address element
- * Add time element
- * Add q element
- * Add international phone input example
- * Add placeholder example
- * Add required field input
- * Add mulitiple select menu
- * Merge HTML5 Form Fields section into Form Fields section
+  * Moved Foundation to external files
+  * Added dropdown headers
+  * Moved docs to external files, added project_name variable
+  * Fixed color & style classes
+  * Style fixes
 
-1.2.0 / 2013-04-02
+1.0.1 / 2014-03-06
 ==================
 
- * Add media query for small viewports
+  * Deleted non-Bootstrap files
+  * Fixed IDs and padding
 
-1.1.0 / 2013-03-22
-==================
 
-  * Remove dependancy on jQuery
-  * Locally host highlight.js files
-  * Use placedog.com instead of lorempixel.com
-
-1.0.0 / 2013-03-06
+1.0.0 / 2014-02-24
 ==================
 
   * Initial commit
--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
-<?php
-	$project_name = "Project Name";
+ <?php
   // Build out URI to reload from form dropdown
   // Need full url for this to work in Opera Mini
   $pageURL = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
 
   if (isset($_POST['sg_uri']) && isset($_POST['sg_section_switcher'])) {
      $pageURL .= $_POST[sg_uri].$_POST[sg_section_switcher];
+     $pageURL = htmlspecialchars( filter_var( $pageURL, FILTER_SANITIZE_URL ) );
      header("Location: $pageURL");
   }
  // Display title of each markup samples as a select option
@@ -20,7 +20,7 @@
 echo '<li role="presentation" class="divider"></li><li role="presentation" class="dropdown-header">'.$type.'</li>';
     sort($files);
     foreach ($files as $file):
-        $filename = preg_replace("/\.html$/i", "", $file); 
+        $filename = preg_replace("/\.html$/i", "", $file);
         $title = preg_replace("/\-/i", " ", $filename);
         $title = ucwords($title);
         echo '<li><a href="#sg-'.$filename.'">'.$title.'</a></li>';
@@ -38,7 +38,7 @@ echo '<li role="presentation" class="divider"></li><li role="presentation" class
 
     sort($files);
     foreach ($files as $file):
-        $filename = preg_replace("/\.html$/i", "", $file); 
+        $filename = preg_replace("/\.html$/i", "", $file);
         $title = preg_replace("/\-/i", " ", $filename);
         $title = ucwords($title);
         echo '<li><a href="#sg-'.$filename.'">'.$title.'</a></li>';
@@ -53,7 +53,7 @@ echo '<li role="presentation" class="divider"></li><li role="presentation" class
               $files[] = $file;
           endif;
       endwhile;
-  
+
       sort($files);
       foreach ($files as $file):
           $filename = preg_replace("/\.html$/i", "", $file);
@@ -102,10 +102,21 @@ echo '<li role="presentation" class="divider"></li><li role="presentation" class
     foreach ($files as $file):
         $filename = preg_replace("/\.html$/i", "", $file);
         $title = preg_replace("/\-/i", " ", $filename);
-        echo '<div class="col-lg-12 sg-section" id="sg-'.$filename.'">';
+        $documentation = 'usage/'.$type.'/'.$file;
+        echo '<div class="sg-markup sg-section">';
         echo '<div class="sg-display">';
-        echo '<h2 class="sg-h2">'.$title.'</h2>';
+        echo '<h2 class="sg-h2"><a id="sg-'.$filename.'" class="sg-anchor">'.$title.'</a></h2>';
+        //echo '<div class="col-md-10 col-md-offset-1">';
+        echo '<div class="row"><div class="col-md-8">';
+        echo '<h3 class="sg-h3">Example</h3>';
         include('markup/'.$type.'/'.$file);
+        echo '</div>';
+         if (file_exists($documentation)) {
+          echo '<div class="col-md-4"><div class="well sg-doc">';
+          echo '<h3 class="sg-h3">Usage Notes</h3>';
+          include($documentation);
+          echo '</div></div></div>';
+        }
         echo '</div><!--/.sg-display-->';
         echo '<div class="sg-markup-controls"><a class="btn btn-primary sg-btn sg-btn--source" href="#">View Source</a> <a class="sg-btn--top" href="#top">Back to Top</a> </div>';
         echo '<div class="sg-source sg-animated">';
@@ -114,73 +125,8 @@ echo '<li role="presentation" class="divider"></li><li role="presentation" class
         echo htmlspecialchars(file_get_contents('markup/'.$type.'/'.$file));
         echo '</code></pre>';
         echo '</div><!--/.sg-source-->';
+        //echo '</div><!--/.colmd10-->';
         echo '</div><!--/.sg-section-->';
     endforeach;
   }
 ?>
-<!DOCTYPE html>
-
-<head>
-<meta charset="utf-8">
-<title><?php echo($project_name)?>Style Guide</title>
-<meta name="viewport" content="width=device-width">
-<!-- Style Guide Boilerplate Styles -->
-<link rel="stylesheet" href="css/styleguide.css">
-<link rel="stylesheet" href="css/bootstrap.css">
-
-<!-- Replace below stylesheet with your own stylesheet -->
-<link rel="stylesheet" href="css/theme.css">
-</head>
-<body   data-spy="scroll" data-target=".navbar-default" data-offset="60" >
-<!-- Fixed navbar -->
-<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-  <div class="container">
-    <div class="navbar-header"> <a class="navbar-brand" href="#"><strong><?php echo($project_name)?></strong> Style Guide</a> </div>
-    <ul class="nav navbar-nav navbar-right">
-      <li class="dropdown active"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Select an element: <b class="caret"></b></a>
-        <ul class="dropdown-menu  scroll-menu sg-sect">
-          <?php listElementsAsOptions('docs'); ?>
-          <?php listElementsAsOptions('foundation'); ?>
-          <li role="presentation" class="divider"></li>
-          <li role="presentation" class="dropdown-header">Base Styles</li>
-          <?php listMarkupAsOptions('base'); ?>
-          <li role="presentation" class="divider"></li>
-          <li role="presentation" class="dropdown-header">Patterns</li>
-          <?php listMarkupAsOptions('patterns'); ?>
-        </ul>
-      </li>
-    </ul>
-  </div>
-</div>
-<!--/.sg-header-->
-
-<div class="sg-body sg-container container">
-<div class="row">
-  <h1 class="page-header">Style Guide Documentation</h1>
-  <?php showDocs(); ?>
-</div>
-<!--/.row-->
-<div class="row">
-  <h1 class="page-header">Foundation</h1>
-  <?php showFoundation(); ?>
-</div>
-<!--/.row-->
-
-<div class="row sg-base-styles">
-  <h1 class="page-header">Base Styles</h1>
-  <?php showMarkup('base'); ?>
-  </div><!--/.sg-base-styles-->
-  <div class="row sg-pattern-styles">
-    <h1 class="page-header">Patterns<small> - Design and mark-up patterns unique to your site.</small></h1>
-    <?php showMarkup('patterns'); ?>
-  </div>
-  <!--/.sg-pattern-styles--> 
-</div>
-<!--/.sg-body--> 
-<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script> 
-<script src="js/scrollspy.js"></script> 
-<script src="js/dropdown.js"></script> 
-<script src="js/sg-plugins.js"></script> 
-<script src="js/sg-scripts.js"></script>
-</body>
-</html>
