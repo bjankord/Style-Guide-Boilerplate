a000d90dce4daffd24d83e601d4524e2de06839d
Brett <bjankord@gmail.com>
2021-11-13 19:00:37 -0600

    Set up static build script

---
+++ sgb.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  styleGuidDir: '', // directory sgb will look in for markup/base, markup/patterns, docs/base, and docs/patterns
+  buildDir: 'build', // generated directory that built files are written to
+};
